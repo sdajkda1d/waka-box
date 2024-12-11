0cfdde5e07770a4deccb5042e93a54db5330d4db
Benjamin <93064761+sdajkda1d@users.noreply.github.com>
2024-12-11 23:33:08 +0800

    Update update_wakatime.py
    将密钥放在本地环境变量里

--- update_wakatime.py
+++ update_wakatime.py
@@ -1,9 +1,11 @@
 import requests
 import time
 
-# 你的 Wakatime API 密钥
-api_key = "your_api_key_here"  # 替换为你的实际 API 密钥
-url = "https://wakatime.com/api/v1/users/current/heartbeats"
+import os
+
+api_key = os.getenv('WAKATIME_API_KEY')
+
+url = "https://wakatime.com/api/v1/users/sdajkda1d/heartbeats"
 
 headers = {
     "Authorization": f"Bearer {api_key}",
@@ -12,7 +14,7 @@ headers = {
 
 # 定义你想提交的活动数据
 data = {
-    "entity": "MyProject",  # 项目名，可以替换成实际的项目名称
+    "entity": "waka-box",  # 项目名，可以替换成实际的项目名称
     "time": int(time.time()),  # 当前时间戳
     "type": "coding",  # 活动类型
     "started": int(time.time() - 300),  # 过去 5 分钟的时间活动
